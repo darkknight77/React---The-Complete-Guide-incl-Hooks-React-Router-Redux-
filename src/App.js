ffcc15191e350b79476fe984cbdd1439428501f7
darkknight77 <shaiklatheefmrf98@gmail.com>
2020-08-14 22:12:47 +0530

    init

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
         <h1>Hello I'm React App</h1>    
       </div>
     ); 
+
     
    //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Is this working?'));
   }
