fb2cf05f06454df212b84db5f0a5f5287bc7d24a
darkknight77 <shaiklatheefmrf98@gmail.com>
2020-08-18 22:10:41 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+ 
+  
   render() {
      return (
       <div className="App">
         <h1>Hello I'm React App</h1>    
+        <p>Is this working?</p>
+        <Person name="John" age="10"/>
+        <Person name="Frank" age="20">My hobbies : Racing</Person>
+        <Person name="George" age="30"/>
       </div>
     ); 
-
-    
+  
    //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Is this working?'));
   }
 }
